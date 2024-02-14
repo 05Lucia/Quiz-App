79008eb18a6f7d40684f5fbb0d39e374a580e72c
Alice Buchholz <alice.buchholz@gmail.com>
2024-02-14 22:57:46 +0100

    multi Questions + css

--- script.js
+++ script.js
@@ -1,98 +1,238 @@
 let questions = [
     {
-        "question": "Who invented HTML?",
-        "answer_1": "Konrad Audenauer",
-        "answer_2": "Miley Cyrus",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Robbie Williams",
-        "right_answer": 3
-    },
-    {
-        "question": "Which HTML element is used to insert a hyperlink into a webpage?",
-        "answer_1": "link",
-        "answer_2": "a",
-        "answer_3": "href",
-        "answer_4": "url",
-        "right_answer": 2
-    },
-    {
-        "question": "What is the correct order of HTML tags for structuring a simple webpage?",
-        "answer_1": "body, header, main, footer",
-        "answer_2": "header, body, footer, main",
-        "answer_3": "header,main, footer, body",
-        "answer_4": "body, footer, header, main",
-        "right_answer": 1
-    },
-    {
-        "question": "What HTML element is used to represent a list of items without a specific order?",
-        "answer_1": "ol",
-        "answer_2": "dl",
-        "answer_3": "li",
-        "answer_4": "ul",
-        "right_answer": 4
-    },
-    {
-        "question": "Which HTML element is used to include an external CSS file into an HTML document?",
-        "answer_1": "style",
-        "answer_2": "script",
-        "answer_3": "meta",
-        "answer_4": "link",
-        "right_answer": 4
-    },
-    {
-        "question": "Which HTML element is used to define a table?",
-        "answer_1": "tr",
-        "answer_2": "table",
-        "answer_3": "td",
-        "answer_4": "th",
-        "right_answer": 2
-    },
-    {
-        "question": "Which HTML element is used to define a paragraph?",
-        "answer_1": "paragraph",
-        "answer_2": "text",
-        "answer_3": "p",
-        "answer_4": "para",
-        "right_answer": 3
-    },
-    {
-        "question": "Which HTML element is used to insert an image into a webpage?",
-        "answer_1": "img",
-        "answer_2": "picture",
-        "answer_3": "image",
-        "answer_4": "src",
-        "right_answer": 1
-    },
-    {
-        "question": "HTML element is used to force a line break within a paragraph?",
-        "answer_1": "nl",
-        "answer_2": "br",
-        "answer_3": "lb",
-        "answer_4": "newline",
-        "right_answer": 2
-    },
-    {
-        "question": "What HTML element is used to mark up a numbered text section?",
-        "answer_1": "ol",
-        "answer_2": "li",
-        "answer_3": "num",
-        "answer_4": "ul",
-        "right_answer": 1
-    },
+        "html": [
+            {
+                "question": "Who invented HTML?",
+                "answer_1": "Konrad Audenauer",
+                "answer_2": "Miley Cyrus",
+                "answer_3": "Tim Berners-Lee",
+                "answer_4": "Robbie Williams",
+                "right_answer": 3
+            },
+            {
+                "question": "Which HTML element is used to insert a hyperlink into a webpage?",
+                "answer_1": "link",
+                "answer_2": "a",
+                "answer_3": "href",
+                "answer_4": "url",
+                "right_answer": 2
+            },
+            {
+                "question": "What is the correct order of HTML tags for structuring a simple webpage?",
+                "answer_1": "body, header, main, footer",
+                "answer_2": "header, body, footer, main",
+                "answer_3": "header,main, footer, body",
+                "answer_4": "body, footer, header, main",
+                "right_answer": 1
+            },
+            {
+                "question": "What HTML element is used to represent a list of items without a specific order?",
+                "answer_1": "ol",
+                "answer_2": "dl",
+                "answer_3": "li",
+                "answer_4": "ul",
+                "right_answer": 4
+            },
+            {
+                "question": "Which HTML element is used to include an external CSS file into an HTML document?",
+                "answer_1": "style",
+                "answer_2": "script",
+                "answer_3": "meta",
+                "answer_4": "link",
+                "right_answer": 4
+            },
+            {
+                "question": "Which HTML element is used to define a table?",
+                "answer_1": "tr",
+                "answer_2": "table",
+                "answer_3": "td",
+                "answer_4": "th",
+                "right_answer": 2
+            },
+            {
+                "question": "Which HTML element is used to define a paragraph?",
+                "answer_1": "paragraph",
+                "answer_2": "text",
+                "answer_3": "p",
+                "answer_4": "para",
+                "right_answer": 3
+            },
+            {
+                "question": "Which HTML element is used to insert an image into a webpage?",
+                "answer_1": "img",
+                "answer_2": "picture",
+                "answer_3": "image",
+                "answer_4": "src",
+                "right_answer": 1
+            },
+            {
+                "question": "HTML element is used to force a line break within a paragraph?",
+                "answer_1": "nl",
+                "answer_2": "br",
+                "answer_3": "lb",
+                "answer_4": "newline",
+                "right_answer": 2
+            },
+            {
+                "question": "What HTML element is used to mark up a numbered text section?",
+                "answer_1": "ol",
+                "answer_2": "li",
+                "answer_3": "num",
+                "answer_4": "ul",
+                "right_answer": 1
+            }
+        ],
+        "css": [
+            {
+                "question": "Which CSS property is used to change the text color?",
+                "answers_1": "font-color",
+                "answers_2": "text-color",
+                "answers_3": "color",
+                "answers_4": "font-style",
+                "right_answer": "3"
+            },
+            {
+                "question": "What does CSS stand for?",
+                "answers_1": "Computer Style Sheets",
+                "answers_2": "Creative Style Sheets",
+                "answers_3": "Cascading Style Sheets",
+                "answers_4": "Complex Style Sheets",
+                "right_answer": "3"
+            },
+            {
+                "question": "Which CSS property is used to change the background color of an element?",
+                "answers_1": "background-color",
+                "answers_2": "color",
+                "answers_3": "bg-color",
+                "answers_4": "background",
+                "right_answer": "1"
+            },
+            {
+                "question": "What is the correct CSS syntax to select an element with id 'example'?",
+                "answers_1": "id: example",
+                "answers_2": ".example",
+                "answers_3": "element.example",
+                "answers_4": "#example",
+                "right_answer": "4"
+            },
+            {
+                "question": "Which CSS property is used to set the spacing between lines of text?",
+                "answers_1": "spacing",
+                "answers_2": "line-height",
+                "answers_3": "line-spacing",
+                "answers_4": "text-spacing",
+                "right_answer": "2"
+            },
+            {
+                "question": "What is the correct CSS syntax to set the font size to 16 pixels?",
+                "answers_1": "font-size: 16px",
+                "answers_2": "text-size: 16px",
+                "answers_3": "size: 16px",
+                "answers_4": "font: 16px",
+                "right_answer": "1"
+            },
+            {
+                "question": "Which CSS property is used to control the size of an element's border?",
+                "answers_1": "border-size",
+                "answers_2": "border-width",
+                "answers_3": "border-style",
+                "answers_4": "border",
+                "right_answer": "2"
+            },
+            {
+                "question": "What is the correct CSS syntax to make a text bold?",
+                "answers_1": "bold: true",
+                "answers_2": "text-weight: bold",
+                "answers_3": "weight: bold",
+                "answers_4": "font-weight: bold",
+                "right_answer": "4"
+            },
+            {
+                "question": "Which CSS property is used to control the spacing between elements?",
+                "answers_1": "margin",
+                "answers_2": "padding",
+                "answers_3": "space",
+                "answers_4": "gap",
+                "right_answer": "1"
+            }
+        ],
+        "javascript": [
+            {
+              "question": "Which keyword is used to declare a variable in JavaScript?",
+              "answers_1": "variable",
+              "answers_2": "let",
+              "answers_3": "const",
+              "answers_4": "var",
+              "right_answer": "4"
+            },
+            {
+              "question": "What is the result of 10 + '5' in JavaScript?",
+              "answers_1": "'105'",
+              "answers_2": "15",
+              "answers_3": "105",
+              "answers_4": "Error",
+              "right_answer": "1"
+            },
+            {
+              "question": "Which function is used to output data in the console?",
+              "answers_1": "log()",
+              "answers_2": "print()",
+              "answers_3": "console.log()",
+              "answers_4": "output()",
+              "right_answer": "3"
+            },
+            {
+              "question": "What is the correct way to write a comment in JavaScript?",
+              "answers_1": "// This is a comment",
+              "answers_2": "<!-- This is a comment -->",
+              "answers_3": "'This is a comment'",
+              "answers_4": "# This is a comment",
+              "right_answer": "1"
+            },
+            {
+              "question": "Which operator is used for strict equality in JavaScript?",
+              "answers_1": "==",
+              "answers_2": "===",
+              "answers_3": "=",
+              "answers_4": "!=",
+              "right_answer": "2"
+            },
+            {
+              "question": "What is the result of typeof null in JavaScript?",
+              "answers_1": "'null'",
+              "answers_2": "'object'",
+              "answers_3": "'undefined'",
+              "answers_4": "'number'",
+              "right_answer": "2"
+            },
+            {
+              "question": "Which loop is used to iterate over an array in JavaScript?",
+              "answers_1": "loop",
+              "answers_2": "while",
+              "answers_3": "foreach",
+              "answers_4": "for",
+              "right_answer": "4"
+            }
+          ]
+    }
 
 ];
 
 let currentQuestion = 0;
 let rightQuestion = 0;
+let quizQuestions = questions
 
-function init() {
+function init(quiz) {
+    quizQuestions = questions[`${quiz}`]
+    document.getElementById('startScrean').style = 'display: none;';
+    document.getElementById('questionBody').style = '';
     document.getElementById('all-questions').innerHTML = questions.length;
-    showQuestion();
+    showQuestion(quiz);
 }
 
-function showQuestion() {
-    
-    let question = questions[currentQuestion];
+function showQuestion(quiz) {
+
+    let question = questions[quiz][currentQuestion];
     document.getElementById('question').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
@@ -127,7 +267,7 @@ function nextQuestion() {
     } else {
 
         showQuestion();
-        
+
         document.getElementById('next-button').disabled = true;
         document.getElementById('current-question').innerHTML = currentQuestion + 1;
 
@@ -139,7 +279,7 @@ function nextQuestion() {
 }
 
 function Progress() {
-    let precent = ((currentQuestion+1)/questions.length)*100;
+    let precent = ((currentQuestion + 1) / questions.length) * 100;
     precent = Math.round(precent)
     document.getElementById('progressbar').innerHTML = `${precent.toFixed(0)}%`;
     document.getElementById('progressbar').style = `width: ${precent}%;`;
