40efaf699e899e5c8b91815e2d787c827dabe566
Alice Buchholz <alice.buchholz@gmail.com>
2024-02-15 12:25:59 +0100

    finished Quiz

--- script.js
+++ script.js
@@ -1,239 +1,265 @@
 
 let htmlQuiz = [
-            {
-                "question": "Who invented HTML?",
-                "answer_1": "Konrad Audenauer",
-                "answer_2": "Miley Cyrus",
-                "answer_3": "Tim Berners-Lee",
-                "answer_4": "Robbie Williams",
-                "right_answer": 3
-            },
-            {
-                "question": "Which HTML element is used to insert a hyperlink into a webpage?",
-                "answer_1": "link",
-                "answer_2": "a",
-                "answer_3": "href",
-                "answer_4": "url",
-                "right_answer": 2
-            },
-            {
-                "question": "What is the correct order of HTML tags for structuring a simple webpage?",
-                "answer_1": "body, header, main, footer",
-                "answer_2": "header, body, footer, main",
-                "answer_3": "header,main, footer, body",
-                "answer_4": "body, footer, header, main",
-                "right_answer": 1
-            },
-            {
-                "question": "What HTML element is used to represent a list of items without a specific order?",
-                "answer_1": "ol",
-                "answer_2": "dl",
-                "answer_3": "li",
-                "answer_4": "ul",
-                "right_answer": 4
-            },
-            {
-                "question": "Which HTML element is used to include an external CSS file into an HTML document?",
-                "answer_1": "style",
-                "answer_2": "script",
-                "answer_3": "meta",
-                "answer_4": "link",
-                "right_answer": 4
-            },
-            {
-                "question": "Which HTML element is used to define a table?",
-                "answer_1": "tr",
-                "answer_2": "table",
-                "answer_3": "td",
-                "answer_4": "th",
-                "right_answer": 2
-            },
-            {
-                "question": "Which HTML element is used to define a paragraph?",
-                "answer_1": "paragraph",
-                "answer_2": "text",
-                "answer_3": "p",
-                "answer_4": "para",
-                "right_answer": 3
-            },
-            {
-                "question": "Which HTML element is used to insert an image into a webpage?",
-                "answer_1": "img",
-                "answer_2": "picture",
-                "answer_3": "image",
-                "answer_4": "src",
-                "right_answer": 1
-            },
-            {
-                "question": "HTML element is used to force a line break within a paragraph?",
-                "answer_1": "nl",
-                "answer_2": "br",
-                "answer_3": "lb",
-                "answer_4": "newline",
-                "right_answer": 2
-            },
-            {
-                "question": "What HTML element is used to mark up a numbered text section?",
-                "answer_1": "ol",
-                "answer_2": "li",
-                "answer_3": "num",
-                "answer_4": "ul",
-                "right_answer": 1
-            }
-        ]
+    {
+        "question": "Who invented HTML?",
+        "answer_1": "Konrad Audenauer",
+        "answer_2": "Miley Cyrus",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Robbie Williams",
+        "right_answer": 3
+    },
+    {
+        "question": "Which HTML element is used to insert a hyperlink into a webpage?",
+        "answer_1": "link",
+        "answer_2": "a",
+        "answer_3": "href",
+        "answer_4": "url",
+        "right_answer": 2
+    },
+    {
+        "question": "What is the correct order of HTML tags for structuring a simple webpage?",
+        "answer_1": "body, header, main, footer",
+        "answer_2": "header, body, footer, main",
+        "answer_3": "header,main, footer, body",
+        "answer_4": "body, footer, header, main",
+        "right_answer": 1
+    },
+    {
+        "question": "What HTML element is used to represent a list of items without a specific order?",
+        "answer_1": "ol",
+        "answer_2": "dl",
+        "answer_3": "li",
+        "answer_4": "ul",
+        "right_answer": 4
+    },
+    {
+        "question": "Which HTML element is used to include an external CSS file into an HTML document?",
+        "answer_1": "style",
+        "answer_2": "script",
+        "answer_3": "meta",
+        "answer_4": "link",
+        "right_answer": 4
+    },
+    {
+        "question": "Which HTML element is used to define a table?",
+        "answer_1": "tr",
+        "answer_2": "table",
+        "answer_3": "td",
+        "answer_4": "th",
+        "right_answer": 2
+    },
+    {
+        "question": "Which HTML element is used to define a paragraph?",
+        "answer_1": "paragraph",
+        "answer_2": "text",
+        "answer_3": "p",
+        "answer_4": "para",
+        "right_answer": 3
+    },
+    {
+        "question": "Which HTML element is used to insert an image into a webpage?",
+        "answer_1": "img",
+        "answer_2": "picture",
+        "answer_3": "image",
+        "answer_4": "src",
+        "right_answer": 1
+    },
+    {
+        "question": "HTML element is used to force a line break within a paragraph?",
+        "answer_1": "nl",
+        "answer_2": "br",
+        "answer_3": "lb",
+        "answer_4": "newline",
+        "right_answer": 2
+    },
+    {
+        "question": "What HTML element is used to mark up a numbered text section?",
+        "answer_1": "ol",
+        "answer_2": "li",
+        "answer_3": "num",
+        "answer_4": "ul",
+        "right_answer": 1
+    }
+]
 let cssQuiz = [
-            {
-                "question": "Which CSS property is used to change the text color?",
-                "answer_1": "font-color",
-                "answer_2": "text-color",
-                "answer_3": "color",
-                "answer_4": "font-style",
-                "right_answer": "3"
-            },
-            {
-                "question": "What does CSS stand for?",
-                "answer_1": "Computer Style Sheets",
-                "answer_2": "Creative Style Sheets",
-                "answer_3": "Cascading Style Sheets",
-                "answer_4": "Complex Style Sheets",
-                "right_answer": "3"
-            },
-            {
-                "question": "Which CSS property is used to change the background color of an element?",
-                "answer_1": "background-color",
-                "answer_2": "color",
-                "answer_3": "bg-color",
-                "answer_4": "background",
-                "right_answer": "1"
-            },
-            {
-                "question": "What is the correct CSS syntax to select an element with id 'example'?",
-                "answer_1": "id: example",
-                "answer_2": ".example",
-                "answer_3": "element.example",
-                "answer_4": "#example",
-                "right_answer": "4"
-            },
-            {
-                "question": "Which CSS property is used to set the spacing between lines of text?",
-                "answer_1": "spacing",
-                "answer_2": "line-height",
-                "answer_3": "line-spacing",
-                "answer_4": "text-spacing",
-                "right_answer": "2"
-            },
-            {
-                "question": "What is the correct CSS syntax to set the font size to 16 pixels?",
-                "answer_1": "font-size: 16px",
-                "answer_2": "text-size: 16px",
-                "answer_3": "size: 16px",
-                "answer_4": "font: 16px",
-                "right_answer": "1"
-            },
-            {
-                "question": "Which CSS property is used to control the size of an element's border?",
-                "answer_1": "border-size",
-                "answer_2": "border-width",
-                "answer_3": "border-style",
-                "answer_4": "border",
-                "right_answer": "2"
-            },
-            {
-                "question": "What is the correct CSS syntax to make a text bold?",
-                "answer_1": "bold: true",
-                "answer_2": "text-weight: bold",
-                "answer_3": "weight: bold",
-                "answer_4": "font-weight: bold",
-                "right_answer": "4"
-            },
-            {
-                "question": "Which CSS property is used to control the spacing between elements?",
-                "answer_1": "margin",
-                "answer_2": "padding",
-                "answer_3": "space",
-                "answer_4": "gap",
-                "right_answer": "1"
-            }
-        ]
+    {
+        "question": "Which CSS property is used to change the text color?",
+        "answer_1": "font-color",
+        "answer_2": "text-color",
+        "answer_3": "color",
+        "answer_4": "font-style",
+        "right_answer": "3"
+    },
+    {
+        "question": "What does CSS stand for?",
+        "answer_1": "Computer Style Sheets",
+        "answer_2": "Creative Style Sheets",
+        "answer_3": "Cascading Style Sheets",
+        "answer_4": "Complex Style Sheets",
+        "right_answer": "3"
+    },
+    {
+        "question": "Which CSS property is used to change the background color of an element?",
+        "answer_1": "background-color",
+        "answer_2": "color",
+        "answer_3": "bg-color",
+        "answer_4": "background",
+        "right_answer": "1"
+    },
+    {
+        "question": "What is the correct CSS syntax to select an element with id 'example'?",
+        "answer_1": "id: example",
+        "answer_2": ".example",
+        "answer_3": "element.example",
+        "answer_4": "#example",
+        "right_answer": "4"
+    },
+    {
+        "question": "Which CSS property is used to set the spacing between lines of text?",
+        "answer_1": "spacing",
+        "answer_2": "line-height",
+        "answer_3": "line-spacing",
+        "answer_4": "text-spacing",
+        "right_answer": "2"
+    },
+    {
+        "question": "What is the correct CSS syntax to set the font size to 16 pixels?",
+        "answer_1": "font-size: 16px",
+        "answer_2": "text-size: 16px",
+        "answer_3": "size: 16px",
+        "answer_4": "font: 16px",
+        "right_answer": "1"
+    },
+    {
+        "question": "Which CSS property is used to control the size of an element's border?",
+        "answer_1": "border-size",
+        "answer_2": "border-width",
+        "answer_3": "border-style",
+        "answer_4": "border",
+        "right_answer": "2"
+    },
+    {
+        "question": "What is the correct CSS syntax to make a text bold?",
+        "answer_1": "bold: true",
+        "answer_2": "text-weight: bold",
+        "answer_3": "weight: bold",
+        "answer_4": "font-weight: bold",
+        "right_answer": "4"
+    },
+    {
+        "question": "Which CSS property is used to control the spacing between elements?",
+        "answer_1": "margin",
+        "answer_2": "padding",
+        "answer_3": "space",
+        "answer_4": "gap",
+        "right_answer": "1"
+    }
+]
 let jsQuiz = [
-            {
-              "question": "Which keyword is used to declare a variable in JavaScript?",
-              "answers_1": "variable",
-              "answer_2": "let",
-              "answer_3": "const",
-              "answer_4": "var",
-              "right_answer": "4"
-            },
-            {
-              "question": "What is the result of 10 + '5' in JavaScript?",
-              "answer_1": "'105'",
-              "answer_2": "15",
-              "answer_3": "105",
-              "answer_4": "Error",
-              "right_answer": "1"
-            },
-            {
-              "question": "Which function is used to output data in the console?",
-              "answer_1": "log()",
-              "answer_2": "print()",
-              "answer_3": "console.log()",
-              "answer_4": "output()",
-              "right_answer": "3"
-            },
-            {
-              "question": "What is the correct way to write a comment in JavaScript?",
-              "answer_1": "// This is a comment",
-              "answer_2": "<!-- This is a comment -->",
-              "answer_3": "'This is a comment'",
-              "answer_4": "# This is a comment",
-              "right_answer": "1"
-            },
-            {
-              "question": "Which operator is used for strict equality in JavaScript?",
-              "answer_1": "==",
-              "answer_2": "===",
-              "answer_3": "=",
-              "answer_4": "!=",
-              "right_answer": "2"
-            },
-            {
-              "question": "What is the result of typeof null in JavaScript?",
-              "answer_1": "'null'",
-              "answer_2": "'object'",
-              "answer_3": "'undefined'",
-              "answer_4": "'number'",
-              "right_answer": "2"
-            },
-            {
-              "question": "Which loop is used to iterate over an array in JavaScript?",
-              "answer_1": "loop",
-              "answer_2": "while",
-              "answer_3": "foreach",
-              "answer_4": "for",
-              "right_answer": "4"
-            }
-          ]
+    {
+        "question": "Which keyword is used to declare a variable in JavaScript?",
+        "answers_1": "variable",
+        "answer_2": "let",
+        "answer_3": "const",
+        "answer_4": "var",
+        "right_answer": "4"
+    },
+    {
+        "question": "What is the result of 10 + '5' in JavaScript?",
+        "answer_1": "'105'",
+        "answer_2": "15",
+        "answer_3": "105",
+        "answer_4": "Error",
+        "right_answer": "1"
+    },
+    {
+        "question": "Which function is used to output data in the console?",
+        "answer_1": "log()",
+        "answer_2": "print()",
+        "answer_3": "console.log()",
+        "answer_4": "output()",
+        "right_answer": "3"
+    },
+    {
+        "question": "What is the correct way to write a comment in JavaScript?",
+        "answer_1": "// This is a comment",
+        "answer_2": "< !-- This is a comment -- >",
+        "answer_3": "'This is a comment'",
+        "answer_4": "# This is a comment",
+        "right_answer": "1"
+    },
+    {
+        "question": "Which operator is used for strict equality in JavaScript?",
+        "answer_1": "==",
+        "answer_2": "===",
+        "answer_3": "=",
+        "answer_4": "!=",
+        "right_answer": "2"
+    },
+    {
+        "question": "What is the result of typeof null in JavaScript?",
+        "answer_1": "'null'",
+        "answer_2": "'object'",
+        "answer_3": "'undefined'",
+        "answer_4": "'number'",
+        "right_answer": "2"
+    },
+    {
+        "question": "Which loop is used to iterate over an array in JavaScript?",
+        "answer_1": "loop",
+        "answer_2": "while",
+        "answer_3": "foreach",
+        "answer_4": "for",
+        "right_answer": "4"
+    }
+]
 
 let currentQuestion = 0;
 let rightQuestion = 0;
 let quizQuestions = 0;
 
-function htmlQuestions () {
+
+
+function htmlQuestions() {
+    document.getElementById('html').classList.add('active');
+    document.getElementById('css').classList.remove('active');
+    document.getElementById('js').classList.remove('active');
+    let disabled = document.getElementById('quiz-screen');
     quizQuestions = htmlQuiz;
+    let html = "HTML";
+    disabled.innerHTML = startTemplate(html);
+    currentQuestion = 0
+    rightQuestion = 0;
 }
 
-function cssQuestions () {
+function cssQuestions() {
+    document.getElementById('css').classList.add('active');
+    document.getElementById('html').classList.remove('active');
+    document.getElementById('js').classList.remove('active');
+    let disabled = document.getElementById('quiz-screen');
     quizQuestions = cssQuiz;
+    let css = "CSS";
+    disabled.innerHTML = startTemplate(css);
+    currentQuestion = 0
+    rightQuestion = 0;
 }
 
-function jsQuestions () {
+function jsQuestions() {
+    document.getElementById('js').classList.add('active');
+    document.getElementById('css').classList.remove('active');
+    document.getElementById('html').classList.remove('active');
+    let disabled = document.getElementById('quiz-screen');
     quizQuestions = jsQuiz;
+    let js = "JavaScript";
+    disabled.innerHTML = startTemplate(js);
+    currentQuestion = 0
+    rightQuestion = 0;
 }
 
 function init() {
-    document.getElementById('startScrean').style = 'display: none;';
-    document.getElementById('questionBody').style = '';
-    document.getElementById('all-questions').innerHTML = quizQuestions.length;
+    let disabled = document.getElementById('quiz-screen');
+    disabled.innerHTML = quwstionTemplate();
+        document.getElementById('all-questions').innerHTML = quizQuestions.length;
     showQuestion();
 }
 
@@ -267,9 +293,10 @@ function answer(selection) {// selection = ausgewählte antwort.
 function nextQuestion() {
     currentQuestion++;
     if (currentQuestion >= quizQuestions.length) {
-        document.getElementById('endScrean').style = '';
-        document.getElementById('questionBody').style = 'display: none;';
+        let disabled = document.getElementById('quiz-screen');
+        disabled.innerHTML = endTemplate();
         score()
+        document.getElementById('main-contaniner'). innerHTML += `<img class="trophy" src="./img/tropy.png" alt="">`; 
     } else {
 
         showQuestion();
---
+++ tempalte.js
@@ -0,0 +1,57 @@
+function startTemplate(a) {
+    return `
+    <div class="card-body start-screen" >
+        <h4>Wlcome to <br> the awesome ${a} quiz!</h4>
+        <p>Ready for the challenge?</p>
+        <button class="btn btn-warning" onclick="init()">Start Now ></button>
+    </div>
+    `;
+}
+
+function quwstionTemplate() {
+    return `
+    <div class="card-body">
+
+    <h5 class="card-title" id="question">Fage</h5>
+
+    <div class="card mb-2 answer" onclick="answer('answer_1')">
+        <div class="card-body" id="answer_1">
+            1
+        </div>
+    </div>
+    <div class="card mb-2  answer" onclick="answer('answer_2')">
+        <div class="card-body" id="answer_2">
+            2
+        </div>
+    </div>
+    <div class="card mb-2 answer" onclick="answer('answer_3')">
+        <div class="card-body" id="answer_3">
+            3
+        </div>
+    </div>
+    <div class="card mb-2 answer" onclick="answer('answer_4')">
+        <div class="card-body" id="answer_4">
+            4
+        </div>
+    </div>
+
+    <div class="question-footer answer">
+        <span><b id="current-question">1</b> von <b id="all-questions">5</b> Fragen</span>
+        <button onclick="nextQuestion()" class="btn btn-secondary green" id="next-button" disabled>nächte Fragen</button>
+    `;
+}
+
+function endTemplate() {
+    return `
+    <div class="card-body end-screen ">
+        <img src="./img/brain result.png"  >
+        <h4>COMPLETE <br> HTML QUIZ</h4>
+        <div class="score" >
+            <h4 class="orange">YOUR SCORE</h4>
+            <h4 id="score">10/10</h4>
+        </div>
+        <button class="btn btn-secondary">SHARE</button>
+        <a href="index.html">New Quiz</a>
+    </div>
+    `;
+}
